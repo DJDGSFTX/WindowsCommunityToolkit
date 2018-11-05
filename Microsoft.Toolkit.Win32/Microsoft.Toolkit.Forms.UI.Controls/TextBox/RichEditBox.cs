7b780709f742476ada13c716ea93865f92a779c2
Peng Zhang (AI AND INK) <pengzh@microsoft.com>
2018-11-05 10:24:48 -0800

    Add more content for winform handWritingTab

---
+++ Microsoft.Toolkit.Win32/Microsoft.Toolkit.Forms.UI.Controls/TextBox/RichEditBox.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Toolkit.Forms.UI.XamlHost;
+using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+
+namespace Microsoft.Toolkit.Forms.UI.Controls
+{
+    public class RichEditBox : WindowsXamlHostBase
+    {
+        internal Windows.UI.Xaml.Controls.RichEditBox UwpControl => GetUwpInternalObject() as Windows.UI.Xaml.Controls.RichEditBox;
+
+        public RichEditBox()
+            : this(typeof(Windows.UI.Xaml.Controls.RichEditBox).FullName)
+        {
+        }
+
+        protected RichEditBox(string name)
+            : base(name)
+        {
+        }
+    }
+}
